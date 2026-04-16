9be64c91773b51e221bc1586f9d0fb5aad88d46a
Siddharth Gupta <132804078+Codingbysid@users.noreply.github.com>
2026-04-16 22:23:34 +0000

    Chapter 8 Submission

--- os/proc.h
+++ os/proc.h
@@ -14,58 +14,61 @@ struct file;
 
 // Saved registers for kernel context switches.
 struct context {
-	uint64 ra;
-	uint64 sp;
+    uint64 ra;
+    uint64 sp;
 
-	// callee-saved
-	uint64 s0;
-	uint64 s1;
-	uint64 s2;
-	uint64 s3;
-	uint64 s4;
-	uint64 s5;
-	uint64 s6;
-	uint64 s7;
-	uint64 s8;
-	uint64 s9;
-	uint64 s10;
-	uint64 s11;
+    // callee-saved
+    uint64 s0;
+    uint64 s1;
+    uint64 s2;
+    uint64 s3;
+    uint64 s4;
+    uint64 s5;
+    uint64 s6;
+    uint64 s7;
+    uint64 s8;
+    uint64 s9;
+    uint64 s10;
+    uint64 s11;
 };
 
 enum threadstate { T_UNUSED, T_USED, SLEEPING, RUNNABLE, RUNNING, EXITED };
 struct thread {
-	enum threadstate state; // Thread state
-	int tid; // Thread ID
-	struct proc *process;
-	uint64 ustack; // Virtual address of user stack
-	uint64 kstack; // Virtual address of kernel stack
-	struct trapframe *trapframe; // data page for trampoline.S
-	struct context context; // swtch() here to run process
-	uint64 exit_code;
+    enum threadstate state; // Thread state
+    int tid; // Thread ID
+    struct proc *process;
+    uint64 ustack; // Virtual address of user stack
+    uint64 kstack; // Virtual address of kernel stack
+    struct trapframe *trapframe; // data page for trampoline.S
+    struct context context; // swtch() here to run process
+    uint64 exit_code;
 };
 
 enum procstate { P_UNUSED, P_USED, ZOMBIE };
 
 // Per-process state
 struct proc {
-	enum procstate state; // Process state
-	int pid; // Process ID
-	pagetable_t pagetable; // User page table
-	uint64 max_page;
-	uint64 ustack_base; // Virtual address of user stack base
-	struct proc *parent; // Parent process
-	uint64 exit_code;
-	//File descriptor table, using to record the files opened by the process
-	struct file *files[FD_BUFFER_SIZE];
-	struct thread threads[NTHREAD];
-	// Use dummy increasing id as index index of lock pool because we don't have destroy method yet
-	uint next_mutex_id, next_semaphore_id, next_condvar_id;
-	struct mutex mutex_pool[LOCK_POOL_SIZE];
-	struct semaphore semaphore_pool[LOCK_POOL_SIZE];
-	struct condvar condvar_pool[LOCK_POOL_SIZE];
-	// LAB5: (1) Define your variables for deadlock detect here.
-	//			 You may need a flag to record if detection enabled,
-	//       and some arrays for detection algorithm.
+    enum procstate state; // Process state
+    int pid; // Process ID
+    pagetable_t pagetable; // User page table
+    uint64 max_page;
+    uint64 ustack_base; // Virtual address of user stack base
+    struct proc *parent; // Parent process
+    uint64 exit_code;
+    //File descriptor table, using to record the files opened by the process
+    struct file *files[FD_BUFFER_SIZE];
+    struct thread threads[NTHREAD];
+    // Use dummy increasing id as index index of lock pool because we don't have destroy method yet
+    uint next_mutex_id, next_semaphore_id, next_condvar_id;
+    struct mutex mutex_pool[LOCK_POOL_SIZE];
+    struct semaphore semaphore_pool[LOCK_POOL_SIZE];
+    struct condvar condvar_pool[LOCK_POOL_SIZE];
+    
+    // LAB5: (1) Variables for deadlock detect
+    int deadlock_detect_enabled;
+    int available[LOCK_POOL_SIZE];
+    int allocation[NTHREAD][LOCK_POOL_SIZE];
+    int request[NTHREAD][LOCK_POOL_SIZE];
 };
 
 int cpuid();
